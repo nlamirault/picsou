944fe4c423dc2db76986db1b1be48860a7a2ccc5
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-12-24 17:58:27 +0100

    Add: wallet command
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- cmd/crypto.go
+++ cmd/crypto.go
@@ -31,7 +31,7 @@ import (
 
 var (
 	name     string
-	names    []string
+	coins    []string
 	limit    int64
 	currency string
 )
@@ -83,14 +83,22 @@ func newCryptoCmd(out io.Writer) *cobra.Command {
 		Use:   "wallet",
 		Short: "Display specific crypto currencies",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			if len(coins) == 0 {
+				return fmt.Errorf("missing crypto currencies names")
+			}
+			client, err := coinmarketcap.NewClient()
+			if err != nil {
+				return err
+			}
+			return cryptoCmd.getCryptoWallet(client, coins, currency)
 		},
 	}
 	listCryptoCmd.PersistentFlags().Int64Var(&limit, "limit", coinmarketcap.DefaultLimit, "Return a maximum of crypto currencies")
 	listCryptoCmd.PersistentFlags().StringVar(&currency, "currency", coinmarketcap.DefaultCurrency, "Default currency to used")
 	getCryptoCmd.PersistentFlags().StringVar(&name, "name", "", "Crypto currency name")
 	getCryptoCmd.PersistentFlags().StringVar(&currency, "currency", coinmarketcap.DefaultCurrency, "Default currency to used")
-	walletCryptoCmd.PersistentFlags().StringSlice("cryptos", nil, "Cryptos' names")
+	walletCryptoCmd.PersistentFlags().StringSliceVar(&coins, "coins", nil, "Coins' names")
+	walletCryptoCmd.PersistentFlags().StringVar(&currency, "currency", coinmarketcap.DefaultCurrency, "Default currency to used")
 	cmd.AddCommand(walletCryptoCmd)
 	cmd.AddCommand(getCryptoCmd)
 	cmd.AddCommand(listCryptoCmd)
@@ -108,9 +116,22 @@ func (cmd cryptoCmd) listCryptoCurrencies(client *coinmarketcap.Client, currency
 
 func (cmd cryptoCmd) getCryptoCurrency(client *coinmarketcap.Client, name string, currency string) error {
 	glog.V(1).Infof("Get crypto currency: %s", name)
-	coins, err := client.GetCoin(name, currency, 1)
+	coin, err := client.GetCoin(name, currency, 1)
 	if err != nil {
 		return err
+	}
+	return cmd.displayCoins(coin, currency)
+}
+
+func (cmd cryptoCmd) getCryptoWallet(client *coinmarketcap.Client, names []string, currency string) error {
+	glog.V(1).Infof("Get crypto wallet: %s", coins)
+	coins := []coinmarketcap.Coin{}
+	for _, name := range names {
+		coin, err := client.GetCoin(name, currency, 1)
+		if err != nil {
+			return err
+		}
+		coins = append(coins, coin...)
 	}
 	return cmd.displayCoins(coins, currency)
 }
